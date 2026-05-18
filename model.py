0965e4137da6b7978450a7e15d82a49872c78d1a
saarthaktiwari-04710 <saarthaktiwari7@gmail.com>
2026-05-18 09:22:50 +0530

    Update model.py

--- model.py
+++ model.py
@@ -72,3 +72,7 @@ class ResNet18(nn.Module):
         return out
 
 model = ResNet18().to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=5e-4)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.1)
